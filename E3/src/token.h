cc9e9ab6ef9d60cb63e6b14af010ac59f4c0c5de
IvonLuiz <ivonluizgarcia@gmail.com>
2024-08-11 23:23:40 -0300

    E3: adding more specific tokens

--- E3/src/token.h
+++ E3/src/token.h
@@ -2,12 +2,29 @@
 
 typedef enum {
         ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
+	NUM,
+        ARRAY,
+        BOOL,
+        /* 260 */
         CHAR,
-	STR,
+        ELSE,
+        FALSE,
+        FOR,
+        FUNC,
+        IF,
+        INT,
+        PRINT,
+        RETURN,
+        STR,
+        /* 270 */
+        TRUE,
+        VOID,
+        WHILE,
+        LTE,
+        GTE,
+        EQU,
+        NEQ,
+        AND,
+        OR,
 	ERROR
-} token_t; 
-
+} token_t;
